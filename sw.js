b97b51f1766d5a70e4baf5adb1dffdb0e9ce6b69
danieladi98 <37324489+danieladi98@users.noreply.github.com>
2019-06-25 17:38:00 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,15 +4,15 @@ self.addEventListener('install', function(event) {
     caches.open('first-app').then(function(cache) {
       //console.log("first app opened");
       return cache.addAll([
-          '/index.html',
-          '/app.js',
-          '/src/js/script.js',
-          '/src/css/style.css',
-          '/src/img/background.jpg',
-          '/src/img/player1.png',
-          '/src/img/logo.png',
-          '/src/manifest.json',
-          '/offline.html'
+//           '/index.html',
+//           '/app.js',
+//           '/src/js/script.js',
+//           '/src/css/style.css',
+//           '/src/img/background.jpg',
+//           '/src/img/player1.png',
+//           '/src/img/logo.png',
+//           '/src/manifest.json',
+//           '/offline.html'
 //           'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
       ]);
     })
