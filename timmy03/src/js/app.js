0cf91bb0bd25a4f24e72b25e60612060ad7f8173
danieladi98 <37324489+danieladi98@users.noreply.github.com>
2019-06-25 14:12:58 +0700

    Update app.js

--- timmy03/src/js/app.js
+++ timmy03/src/js/app.js
@@ -3,7 +3,7 @@ if (!window.Promise) {
 }
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function () {
+  navigator.serviceWorker.register('sw.js').then(function () {
     console.log('Service worker registered!');
     })
     .catch(function(err) {
